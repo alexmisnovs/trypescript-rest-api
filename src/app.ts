a8b305c419bcd34136202b9248661aef3f717b61
Alex macbookAir <aleksandrmisnov@gmail.com>
2023-07-11 08:00:13 +0100

    added zod schema validate request

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
+import routes from "./routes";
 
 const app = express();
 
@@ -10,4 +11,6 @@ const port = config.get<number>("port");
 app.listen(port, async () => {
   logger.info(`App is running on http://localhost:${port}`);
   await connect();
+
+  routes(app);
 });
