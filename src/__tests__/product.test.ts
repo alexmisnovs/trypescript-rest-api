08b6e5c1e10e2ecf8ce66f9027f85fa4691dc8dc
Alex macbookAir <aleksandrmisnov@gmail.com>
2023-07-14 18:57:56 +0100

    finished the product tests

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -4,6 +4,8 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { createProduct } from "../services/product.service";
 import { IProductInput } from "../models/product.model";
+import { signJWT } from "../utils/jwt";
+import { IUserInput } from "../models/user.model";
 
 const mockUserId = new mongoose.Types.ObjectId().toString();
 
@@ -14,6 +16,15 @@ export const mockProductPayload: IProductInput = {
     "Designed for first-time DSLR owners who want impressive results straight out of the box, capture those magic moments no matter your level with the EOS 1500D. With easy to use automatic shooting modes, large 24.1 MP sensor, Canon Camera Connect app integration and built-in feature guide, EOS 1500D is always ready to go.",
   image: "https://i.imgur.com/QlRphfQ.jpg",
   price: 699.99,
+};
+
+interface IMockUser extends Omit<IUserInput, "password"> {
+  _id: string;
+}
+export const mockUserPayload: IMockUser = {
+  _id: mockUserId,
+  email: "jane.doe@app.com",
+  name: "Jane Doe",
 };
 
 const app = createServer();
@@ -57,11 +68,40 @@ describe("product", () => {
       });
     });
   });
+
   describe("create product route", () => {
     describe("given the user is not logged in", () => {
       it("should return a 403", async () => {
         const { statusCode } = await supertest(app).post(`/api/products/`);
         expect(statusCode).toBe(403);
+      });
+    });
+    // can use skip() or only() on both describe and it if necessary
+    describe("given the user is logged in", () => {
+      it("should return a 200 and a product", async () => {
+        // sign JWT
+        const jwt = signJWT(mockUserPayload);
+
+        const { body, statusCode } = await supertest(app)
+          .post(`/api/products/`)
+          .set("Authorization", `Bearer ${jwt}`)
+          .send(mockProductPayload);
+
+        expect(statusCode).toBe(200);
+
+        expect(body).toEqual({
+          __v: 0,
+          _id: expect.any(String),
+          createdAt: expect.any(String),
+          description:
+            "Designed for first-time DSLR owners who want impressive results straight out of the box, capture those magic moments no matter your level with the EOS 1500D. With easy to use automatic shooting modes, large 24.1 MP sensor, Canon Camera Connect app integration and built-in feature guide, EOS 1500D is always ready to go.",
+          image: "https://i.imgur.com/QlRphfQ.jpg",
+          price: 699.99,
+          productId: expect.any(String),
+          title: "Canon EOS 1500D DSLR Camera with 18-55mm Lens",
+          updatedAt: expect.any(String),
+          user: expect.any(String),
+        });
       });
     });
   });
